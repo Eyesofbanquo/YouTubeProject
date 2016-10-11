2bbc20cac2cb87fb3002eecc8a92a9455e4ab805
Eyesofbanquo <mls103020@utdallas.edu>
2016-10-11 14:16:04 -0500

    first commit

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'YouTube' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for YouTube
+  pod 'SwiftyJSON'
+  pod 'Alamofire'
+  pod 'Quick'
+  pod 'Nimble'
+
+  target 'YouTubeTests' do
+    inherit! :search_paths
+  end
+
+end
---
+++ YouTube/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  YouTube
+//
+//  Created by Markim Shaw on 10/10/16.
+//  Copyright © 2016 Markim Shaw. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
